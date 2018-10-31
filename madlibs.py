102774f94902990634582bd26e263b263f681036
Analicia Abernathy <analicia.abernathy@ibm.com>
2018-10-31 09:24:05 -0500

    aestetics
    added line breaks to show on the console neater

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,4 @@
+
 """
 This program generates a story based off the word provided.  Similar to the popular MadLibs
 Author: Analicia 
@@ -6,30 +7,30 @@ Author: Analicia
 
 story = "This morning %s woke up feeling %s. 'It is going to be a %s day!' Outside, a bunch of %s's were protesting to keep %s in stores. They began to %s to the rhythm of the %s, which made all the %s's very %s. Concerned, %s texted %s, who flew %s to %s and dropped %s in a puddle of frozen %s. %s woke up in the year %s, in a world where %s's ruled the world."
 
-print "~ MadLib Stories ~"
+print ("~ MadLib Stories ~\n")
 
-print "First lets get your name."
-name = raw_input("Enter your name: ")
+print ("First lets get your name.")
+name = input("Enter your name: ")
 
-print "Welcome to MadLibs " + str(name) + ". Please provided the requested number of adjectives, verbs and nouns to present your completed story."
+print ("\nWelcome to MadLibs " + (name) + ". Please provided the requested number of adjectives, verbs and nouns to present your completed story.")
 
-adj1 = raw_input("Adjective 1: ")
-adj2 = raw_input("Adjective 2: ")
-adj3 = raw_input("Adjective 3: ")
+adj1 = input("Adjective 1: ")
+adj2 = input("Adjective 2: ")
+adj3 = input("Adjective 3: ")
 
-verb1 = raw_input("Verb 1: ")
+verb1 = input("\nVerb 1: ")
 
-noun1 = raw_input("Noun 1: ")
-noun2 = raw_input("Noun 2: ")
+noun1 = input("\nNoun 1: ")
+noun2 = input("Noun 2: ")
 
-print "Let's change things up a little bit."
+print ("\nLet's change things up a little bit.")
 
-animal = raw_input("What is your favorite animal: ")
-food = raw_input("What is your favorite food: ")
-fruit = raw_input("What is your favorite fruit: ")
-superhero = raw_input("Who is your favorite superhero: ")
-country = raw_input("What is a country that you have always wanted to visit: ")
-dessert = raw_input("What is your favorite dessert: ")
-year = raw_input("What year are you born in: ")
-
-print story % (name, adj1, adj2, animal, food, verb1, noun1, fruit, adj3, name, superhero, name, country, name, dessert, name, year, noun2)
+animal = input("What is your favorite animal: ")
+food = input("What is your favorite food: ")
+fruit = input("What is your favorite fruit: ")
+superhero = input("Who is your favorite superhero: ")
+country = input("What is a country that you have always wanted to visit: ")
+dessert = input("What is your favorite dessert: ")
+year = int(input("What year are you born in: "))
+print ("")
+print (story % (name, adj1, adj2, animal, food, verb1, noun1, fruit, adj3, name, superhero, name, country, name, dessert, name, year, noun2))
