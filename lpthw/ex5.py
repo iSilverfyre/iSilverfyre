d6e15f179132a35aa0ef4985904f1a37fb099f1e
Analicia Abernathy <analicia.abernathy@ibm.com>
2018-10-31 16:10:55 -0500

    more practice

--- lpthw/ex5.py
+++ lpthw/ex5.py
@@ -1,9 +1,18 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 #lbs
-my_eyes = 'Blue' 
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 #lbs
+eyes = 'Blue' 
+teeth = 'White'
+hair = 'Brown'
 
-print (f"Lets talk about {my_name}.")
+print (f"Let's talk about {name}.")
+print (f"He's {height} inches tall.")
+print (f"He's {weight} pounds heavy.")
+print ("Actually that's not too heavy.")
+print (f"He's got {eyes} eyes and {hair} hair.")
+print (f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
