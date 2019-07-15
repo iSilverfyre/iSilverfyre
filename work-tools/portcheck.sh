a8b3e55a08ff4d256759374f8543ed375197b099
Analicia Abernathy <cah.gurl.4evr@gmail.com>
2019-07-15 11:21:43 -0500

    Delete portcheck.sh

--- work-tools/portcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-echo "==========================================="
-echo "Compute SRE Port Check Tool"
-echo "==========================================="
-echo "What is the IP address of the server you are working with?"
-read ipaddr
-ssh root@$ipaddr < ~/.port
