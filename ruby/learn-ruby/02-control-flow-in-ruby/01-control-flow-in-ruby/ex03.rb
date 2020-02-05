5895f5a0219cb8a79ee4f4f262d1175f85988378
Analicia Abernathy <analicia.abernathy1@ibm.com>
2020-02-04 22:02:11 -0600

    Create ex03.rb

---
+++ ruby/learn-ruby/02-control-flow-in-ruby/01-control-flow-in-ruby/ex03.rb
@@ -0,0 +1,13 @@
+print "Please provide a number for x. "
+num_x = gets.chomp
+
+print "Please provide a number for y. "
+num_y = gets.chomp
+
+if num_x > num_y
+  puts "#{num_x} is greater than #{num_y}!"
+elsif num_x < num_y
+  puts "#{num_x} is less than #{num_y}!"
+else
+  puts "#{num_x} equals #{num_y}"
+end
