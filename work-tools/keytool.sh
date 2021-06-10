e32e53cfda870762a65fab90d1a3303378d5e3d7
Analicia Abernathy <cah.gurl.4evr@gmail.com>
2021-06-10 13:01:05 -0500

    Update keytool.sh

--- work-tools/keytool.sh
+++ work-tools/keytool.sh
@@ -2,32 +2,64 @@
 
 clear
 
-printf "Moving to YubiKey CLI Directory"
+echo "Moving to YubiKey CLI Directory"
 cd /Applications/YubiKey\ Manager.app/Contents/MacOS/
 
-printf "\nCollecting Serial Information & Generating Key"
+echo ""
+echo "Collecting Serial Information & Generating Key"
 SERIAL=`./ykman list -s`
 HEX=`echo "obase=16; $SERIAL" |bc`
 KEY=000000$HEX
-printf $KEY
+echo "Serial - "$SERIAL
+echo "Key - "$KEY
 
-printf "\nUnlocking Slot 1"
+echo ""
+echo "Unlocking Slot 1"
 ./ykman otp --access-code $KEY settings --delete-access-code 1
 
-printf "\nSetting Static Password to Slot 2"
+echo ""
+echo "Setting Static Password to Slot 2"
 ./ykman otp static -g -l 12 2
 
-printf "\nEnabling FIDO"
+echo ""
+echo "Enabling FIDO"
 ./ykman config mode o+f
 read -p "Press enter after YubiKey has been reinserted."
 
-printf "\nLocking Slot 1"
+echo ""
+echo "Locking Slot 1"
 ./ykman otp --access-code $KEY settings 1
 
-printf "\nLocking Slot 2"
+echo ""
+echo "Locking Slot 2"
 ./ykman otp --access-code $KEY settings 2
 
-printf "\nLocking FIDO"
+echo ""
+echo "Locking FIDO"
+./ykman fido access change-pin --u2f -n $KEY
 
-printf "\n\nValidation"
-./ykman otp info && ./ykman fido info
+echo ""
+echo "Validating Key"
+echo "UBHE"$SERIAL" FIPS" >> ~/Desktop/serial-log.txt
+echo "UBHE"$SERIAL" FIPS" >> ~/Desktop/validaton-verification.txt
+
+echo ""
+echo "OTP Validation"
+./ykman otp info
+
+echo ""
+echo "FIDO Validation"
+./ykman fido info
+
+echo ""
+echo "Saving Validation Logs"
+echo "Saving OTP Log"
+echo "OPT Log" >> ~/Desktop/validaton-verification.txt
+./ykman otp info >> ~/Desktop/validaton-verification.txt
+
+echo "Saving FIDO Log"
+echo "OPT Log" >> ~/Desktop/validaton-verification.txt
+./ykman fido info; echo "" >>~/Desktop/validaton-verification.txt
+
+echo ""
+echo "Key Complete - Please Remove" $SERIAL
