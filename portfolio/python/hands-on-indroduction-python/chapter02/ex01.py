d62dc8a787057ae96e74913f98ff4727a9c23da3
Analicia Abernathy <cah.gurl.4evr@gmail.com>
2025-08-16 14:16:54 -0500

    Create ex01.py

---
+++ portfolio/python/hands-on-indroduction-python/chapter02/ex01.py
@@ -0,0 +1,15 @@
+RUN_INDENTED = True
+
+message = "running unindented"
+
+if RUN_INDENTED:
+    message = "running indented"
+
+print(message)
+
+
+def my_function():
+    greet = "Hello"
+    return greet
+
+print(my_function())
